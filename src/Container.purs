67efd9c054d2ffe2282bc72b0425898f7f2d0265
John Watson <john.watson@gmx.co.uk>
2019-03-01 11:46:11 +0000

    Upgrade to Halogen 5 Release Candidate 1

--- src/Container.purs
+++ src/Container.purs
@@ -7,22 +7,20 @@ import Audio.SoundFont.Melody.Class (MidiRecording(..))
 import Data.Abc (AbcTune)
 import Data.Abc.Midi (toMidi)
 import Data.Abc.Parser (PositionedParseError)
-import Lessons as Lessons
 import Data.Array (singleton) as A
 import Data.Either (Either(..), either)
-import Data.Either.Nested (Either2)
-import Data.Functor.Coproduct.Nested (Coproduct2)
 import Data.List (List(..))
 import Data.Maybe (Maybe(..))
+import Data.Symbol (SProxy(..))
 import Effect.Aff (Aff)
 import Halogen as H
-import Halogen.Component.ChildPath as CP
 import Halogen.EditorComponent as ED
 import Halogen.HTML as HH
 import Halogen.HTML.Core (ClassName(..))
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import Halogen.PlayerComponent as PC
+import Lessons as Lessons
 
 type State =
   { instruments :: Array Instrument
@@ -30,11 +28,11 @@ type State =
   , lessonIndex :: Int
   }
 
-data Query a =
-    Init a
-  | HandleNewTuneText ED.Message a
-  | Move Int a          -- Move lesson to this number
-  | HandleTuneIsPlaying PC.Message a
+data Action =
+    Move Int
+  | Init
+  | HandleNewTuneText ED.Message
+  | HandleTuneIsPlaying PC.Message
 
 data MoveButtonType =
     Start
@@ -50,40 +48,35 @@ emptyTune =
 -- so we must specialize to MidiRecording
 type PlayerQuery = PC.Query MidiRecording
 
-type ChildQuery = Coproduct2 ED.Query PlayerQuery
+type ChildSlots =
+  ( editor :: ED.Slot Unit
+  , player :: (PC.Slot MidiRecording) Unit
+  )
 
--- slots and slot numbers
-type PlayerSlot = Unit
-type EditorSlot = Unit
+_editor = SProxy :: SProxy "editor"
+_player = SProxy :: SProxy "player"
 
-type ChildSlot = Either2 Unit Unit
-
-editorSlotNo :: CP.ChildPath ED.Query ChildQuery EditorSlot ChildSlot
-editorSlotNo = CP.cp1
-
-playerSlotNo :: CP.ChildPath PlayerQuery ChildQuery PlayerSlot ChildSlot
-playerSlotNo = CP.cp2
-
-component ::  H.Component HH.HTML Query Unit Void Aff
+component ::  forall q i o. H.Component HH.HTML q i o Aff
 component =
-  H.lifecycleParentComponent
-    { initialState: const initialState
+  H.mkComponent
+    { initialState
     , render
-    , eval
-    , initializer: Just (H.action Init)
-    , finalizer: Nothing
-    , receiver: const Nothing
+    , eval: H.mkEval $ H.defaultEval
+        { handleAction = handleAction
+        , initialize = Just Init
+        , finalize = Nothing
+        }
     }
   where
 
-  initialState :: State
-  initialState =
+  initialState :: i -> State
+  initialState _ =
     { instruments: []
     , tuneResult: ED.nullTune
     , lessonIndex : 0
     }
 
-  render :: State -> H.ParentHTML Query ChildQuery ChildSlot Aff
+  render :: State -> H.ComponentHTML Action ChildSlots Aff
   render state = HH.div_
     [ HH.h1
          [HP.class_ (H.ClassName "center") ]
@@ -103,7 +96,7 @@ component =
           [ HH.div
              [ HP.class_ (H.ClassName "leftPanelComponent") ]
                [
-                 HH.slot' editorSlotNo unit ED.component unit (HE.input HandleNewTuneText)
+                 HH.slot _editor unit ED.component unit (Just <<< HandleNewTuneText)
                ]
           , HH.div
             [ HP.class_ (H.ClassName "leftPanelComponent") ]
@@ -133,44 +126,40 @@ component =
             ]
       ]
 
-
-  eval :: Query ~> H.ParentDSL State Query ChildQuery ChildSlot Void Aff
-  eval (Init next) = do
-    instrument <- H.liftAff $ loadPianoSoundFont "assets/soundfonts"
-    _ <- H.query' editorSlotNo unit $ H.action (ED.UpdateContent (Lessons.example 0))
-    _ <- H.modify (\st -> st { instruments = A.singleton instrument } )
-    pure next
-  eval (HandleNewTuneText (ED.TuneResult r) next) = do
-    _ <- refreshPlayerState r
-    let
-      abcTune = either (\_ -> emptyTune) (identity) r
-    _ <- H.modify (\st -> st { tuneResult = r } )
-    pure next
-  eval (Move lessonIndex next) = do
-    _ <- H.modify (\st -> st { lessonIndex = lessonIndex } )
-    _ <- H.query' editorSlotNo unit $ H.action (ED.UpdateContent (Lessons.example lessonIndex))
-    _ <- H.query' playerSlotNo unit $ H.action PC.StopMelody
-    pure next
-  eval (HandleTuneIsPlaying (PC.IsPlaying p) next) = do
-    -- we ignore this message, but if we wanted to we could
-    -- disable any button that can alter the editor contents whilst the player
-    -- is playing and re-enable when it stops playing
-    {-
-    _ <- H.query' editorSlotNo unit $ H.action (ED.UpdateEnabled (not p))
-    _ <- H.query' clearTextSlotNo unit $ H.action (Button.UpdateEnabled (not p))
-    -}
-    pure next
+  handleAction ∷ Action → H.HalogenM State Action ChildSlots o Aff Unit
+  handleAction = case _ of
+    Init -> do
+      instrument <- H.liftAff $ loadPianoSoundFont "assets/soundfonts"
+      _ <- H.query _editor unit $ H.tell (ED.UpdateContent (Lessons.example 0))
+      _ <- H.modify (\st -> st { instruments = A.singleton instrument } )
+      pure unit
+    Move lessonIndex -> do
+      _ <- H.modify (\st -> st { lessonIndex = lessonIndex } )
+      _ <- H.query _editor unit $ H.tell (ED.UpdateContent (Lessons.example lessonIndex))
+      _ <- H.query _player unit $ H.tell PC.StopMelody
+      pure unit
+    HandleNewTuneText (ED.TuneResult r) -> do
+      _ <- refreshPlayerState r
+      let
+        abcTune = either (\_ -> emptyTune) (identity) r
+      _ <- H.modify (\st -> st { tuneResult = r } )
+      pure unit
+    HandleTuneIsPlaying message -> do
+      -- we ignore this message, but if we wanted to we could
+      -- disable any button that can alter the editor contents whilst the player
+      -- is playing and re-enable when it stops playing
+      pure unit
 
 -- refresh the state of the player by passing it the tune result
 -- (if it had parsed OK)
-refreshPlayerState ::
+refreshPlayerState :: ∀ o.
        Either PositionedParseError AbcTune
-    -> H.ParentDSL State Query ChildQuery ChildSlot Void Aff Unit
+    -> H.HalogenM State Action ChildSlots o Aff Unit
 refreshPlayerState tuneResult = do
   _ <- either
-    (\_ -> H.query' playerSlotNo unit $ H.action (PC.StopMelody))
-    (\abcTune -> H.query' playerSlotNo unit $ H.action (PC.HandleNewPlayable (toPlayable abcTune)))
-    tuneResult
+     (\_ -> H.query _player unit $ H.tell PC.StopMelody)
+     (\abcTune -> H.query _player unit $ H.tell (PC.HandleNewPlayable (toPlayable abcTune)))
+     tuneResult
   pure unit
 
 -- helpers
@@ -181,19 +170,21 @@ toPlayable abcTune =
    MidiRecording $ toMidi abcTune
 
 -- rendering functions
-renderPlayer ::  State -> H.ParentHTML Query ChildQuery ChildSlot Aff
+renderPlayer :: State -> H.ComponentHTML Action ChildSlots Aff
 renderPlayer state =
   case state.tuneResult of
     Right abcTune ->
       HH.div
         [ HP.class_ (H.ClassName "leftPanelComponent")]
-        [ HH.slot' playerSlotNo unit (PC.component (toPlayable abcTune) state.instruments) unit (HE.input HandleTuneIsPlaying)  ]
+        [
+          HH.slot _player unit (PC.component (toPlayable abcTune) state.instruments) unit (Just <<< HandleTuneIsPlaying)
+        ]
     Left err ->
       HH.div_
         [  ]
 
 -- | render any of the four buttons that move the lesson index
-renderMoveIndexButton :: MoveButtonType -> State -> H.ParentHTML Query ChildQuery ChildSlot Aff
+renderMoveIndexButton :: MoveButtonType -> State -> H.ComponentHTML Action ChildSlots Aff
 renderMoveIndexButton buttonType state =
   let
     enabled =
@@ -238,7 +229,7 @@ renderMoveIndexButton buttonType state =
           "last"
   in
     HH.button
-      [ HE.onClick (HE.input_ (Move targetIndex))
+      [ HE.onClick  \_ -> Just (Move targetIndex)
       , HP.class_ $ ClassName className
       , HP.enabled enabled
       ]
